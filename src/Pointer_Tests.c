b604439a66e1ccc99b4dc5e3b34384effbd7f8bf
quarr <quarr@192.168.137.48>
2019-05-08 11:55:38 +1000

    New repository for pointer testing.

---
+++ src/Pointer_Tests.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Pointer_Tests.c
+ Author      : David Procter
+ Version     : 000
+ Copyright   : Copyright - David Procter
+ Description : Pointer testing for variables and arrays
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Test No1"); /* prints Test No1 */
+	return EXIT_SUCCESS;
+}
